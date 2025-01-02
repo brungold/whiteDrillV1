7d15036a39ace87b59c0fd418fc7e1e48f2f105e
brungold <maciej.m.mysliwiec@gmail.com>
2025-01-02 01:08:08 +0100

    added swagger part to the securityFilterChain

--- src/main/java/pl/whitedrillv1/infrastructure/security/SecurityConfig.java
+++ src/main/java/pl/whitedrillv1/infrastructure/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import pl.whitedrillv1.domain.usercrud.UserRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 class SecurityConfig {
@@ -25,6 +30,7 @@ class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
@@ -33,22 +39,27 @@ class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable());
+        http.cors(corsConfigurerCustomizer());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/users/register/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/dentist/**").permitAll()
-                .requestMatchers(HttpMethod.POST,"/appointments/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/appointments/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PATCH,"/appointments/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST,"/schedule/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET,"/schedule/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET,"/patients/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.POST,"/patients/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/dentist/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/appointments/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/appointments/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/appointments/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/schedule/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/schedule/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/patients/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/patients/**").hasRole("ADMIN")
                 .anyRequest().authenticated());
         return http.build();
     }
-//            , JwtAuthTokenFilter jwtAuthTokenFilter) throws Exception {
+
+    //            , JwtAuthTokenFilter jwtAuthTokenFilter) throws Exception {
 //
 //        //http.csrf(Customizer.withDefaults());
 //        http.cors(corsConfigurerCustomizer());
@@ -81,4 +92,19 @@ class SecurityConfig {
 //                .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
 //                .anyRequest().authenticated());
 //        return http.build();
+    public Customizer<CorsConfigurer<HttpSecurity>> corsConfigurerCustomizer() {
+        return c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        List.of("https://localhost:3000"));
+                config.setAllowedMethods(
+                        List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+                return config;
+            };
+            c.configurationSource(source);
+        };
+    }
 }
